c9a95aa66a74d7d79f124182b0dbc1b1ef7d8f46
lauraginer <lauginmat@gmail.com>
2025-12-12 22:17:53 +0100

    Still working on syntax on cpp's exercises, now that i have, is done and works :)

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,7 +6,25 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 20:36:14 by lginer-m          #+#    #+#             */
-/*   Updated: 2025/12/11 20:45:18 by lginer-m         ###   ########.fr       */
+/*   Updated: 2025/12/12 22:13:24 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	
+	if(argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while(i < argc)
+		{
+			std::cout << argv[i] << std::endl; //salto de linea no, debe de ser espacio (solo despues del programa)
+			i++;
+		}
+	}
+	return(0);
+}
