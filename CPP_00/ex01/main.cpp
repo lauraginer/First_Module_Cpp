729ae2722733eba2b451629730764b4dab34ba97
lauraginer <lauginmat@gmail.com>
2025-12-13 22:05:32 +0100

    Exercise ex00 is ready! Still working on phonebook, need to study about ::string and classes about CPP, be careful with C fuctions (are forbidden) and what means private and public. Still working!!

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -1,39 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/11 20:36:14 by lginer-m          #+#    #+#             */
-/*   Updated: 2025/12/13 21:01:56 by lginer-m         ###   ########.fr       */
+/*   Created: 2025/12/13 21:14:56 by lginer-m          #+#    #+#             */
+/*   Updated: 2025/12/13 22:00:58 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char **argv)
 {
-	int i = 1;
-	int j;
-	char result;
-	
-	if(argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
+	if(argc == 2)
 	{
-		while(i < argc)
-		{
-			j = 0;
-			while(argv[i][j])
-			{
-				result = toupper(argv[i][j]);
-				std::cout << result;
-				j++;
-			}	
-			i++;
-		}
+		std::string cmd = argv[1];
+		if(cmd == "ADD")
+			std::cout << "ADD";
+		else if(cmd == "SEARCH")
+			std::cout << "SEARCH";
+		else if(cmd == "EXIT")
+			exit(0);
 	}
+	else
+		std::cout << "Please, enter one option: ADD, SEARCH or EXIT";
 	std::cout << std::endl;
 	return(0);
 }
