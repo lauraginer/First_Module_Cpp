134bf8768b4a22a4d5f0aa819d519460320965eb
lauraginer <lauginmat@gmail.com>
2026-01-23 18:59:08 +0100

    In theory, we still need to implement the last few functions, check that everything is working correctly, and review it because it's a bit confusing. Good luck!

--- CPP_02/ex02/Fixed.cpp
+++ CPP_02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 19:48:56 by lginer-m          #+#    #+#             */
-/*   Updated: 2026/01/22 15:53:24 by lginer-m         ###   ########.fr       */
+/*   Updated: 2026/01/23 18:56:20 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,12 +72,13 @@ int Fixed::toInt( void ) const
 	real_value = value >> bits;
 	return(real_value);
 }
+
+//Funciones de sobrecarga de operadores
 std::ostream &operator<<(std::ostream& out, Fixed const& obj)
 {
 	out << obj.toFloat();
     return(out);
 }
-
 bool Fixed::operator<(Fixed const& obj) const
 {
 	bool flag;
@@ -114,6 +115,8 @@ bool Fixed::operator!=(Fixed const& obj) const
 	flag = this->value != obj.value;
 	return(flag);
 }
+
+//Funciones aritmeticas
 Fixed Fixed::operator+(Fixed const& obj) const
 {
 	Fixed result;
@@ -126,15 +129,45 @@ Fixed Fixed::operator-(Fixed const& obj) const
 	result.value = this->value - obj.value;
 	return(result);
 }
-Fixed Fixed::operator*(Fixed const& obj) const
-{
+Fixed Fixed::operator*(Fixed const& obj) const //Se puede convertir todas estas funiones aritmeticas a flost,
+{											 //Al parecer hay que desplazar los bits para evitar numeros desbordados (sino se multiplican dos veces)
 	Fixed result;
-	result.value = this->value * obj.value;
+	result.value = (this->value * obj.value) >> bits;
 	return(result);
 }
 Fixed Fixed::operator/(Fixed const& obj) const
 {
 	Fixed result;
-	result.value = this->value / obj.value;
+	result.value = (this->value << bits) / obj.value;
 	return(result);
+}
+
+//Funciones de decremento/incremento
+Fixed& Fixed::operator++()
+{
+	this->value++;
+	return(*this);
+}
+Fixed Fixed::operator++(int)
+{
+	Fixed current_value(*this); //Copia todo el objeto
+	this->value++;
+	return(current_value);
+}
+Fixed Fixed::operator--(int)
+{
+	Fixed current_value(*this); //Copia todo el objeto
+	this->value--;
+	return(current_value);
+}
+Fixed& Fixed::operator--()
+{
+	this->value--;
+	return(*this);
+}
+
+static Fixed& min(Fixed& a, Fixed& b)
+{
+	//Devuelve una referencia al menor
+	
 }
